39dae94fcc0df04fa76ca2d96e9692df5638ff3f
ychen-1202 <ychen@prometheum.com>
2020-05-14 11:31:42 -0400

    Sort out code file

--- EXXAccounts_api.js
+++ EXXAccounts_api.js
@@ -1,33 +1,33 @@
-// var fs = require("fs");
 var java = require("java");
-// var baseDir = "./lib";
-// var dependencies = fs.readdirSync(baseDir);
+
 java.classpath.push("commons-lang3-3.1.jar");
 java.classpath.push("commons-io-2.4.jar");
-// java.classpath.push("EXXAccounts_api.class");
-
-// dependencies.forEach(function(dependency) {
-//     console.log(dependency)
-//     java.classpath.push(dependency);
-// })
-java.classpath.push("Test.class");
-
-// var req = java.import("org.web3j.protocol.core.methods.request");
 
 var ArrayList = java.import('java.util.ArrayList');
 var arrlist = new ArrayList();
 arrlist.addSync('item2');
 console.log(arrlist.toStringSync());
 
+java.classpath.push("./sstContracts/Test.class");
+
 var Test = java.import('sstContracts.Test');
+
+// var fs = require("fs");
+// var baseDir = "./lib";
+// var dependencies = fs.readdirSync(baseDir);
+
+// dependencies.forEach(function(dependency) {
+//     console.log(dependency)
+//     java.classpath.push(dependency);
+// })
+
+// java.classpath.push("EXXAccounts_api.class");
 
 // java.newInstanceSync("Transaction");
 
 // java.classpath.push("/home/yunchen/Projects/wxprm-PlatONE-Go/example_sdk/test/src");
 
-// var trans = java.import("src/sstContracts/EXXAccounts_api");
-
-// /sstContracts/EXXAccounts_api
+// var EXXAccounts_api = java.import("sstContracts/EXXAccounts_api");
 
 // var EXXAccounts = java.newInstanceSync("sstContracts.EXXAccounts_api");
 
---
+++ sstContracts/Test.java
@@ -0,0 +1,12 @@
+package sstContracts;
+
+public class Test {
+    public void print() {
+        System.out.println("print out...");
+    }
+
+    // public static void main(String[] args) {
+    //     Test t = new Test();
+    //     t.print();
+    // }
+}
